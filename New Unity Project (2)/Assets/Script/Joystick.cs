37e067edfc773aeb625537f5bb51d0edd197d3cf
WonJongHyeok <63942325+WonJongHyeok@users.noreply.github.com>
2020-05-17 15:51:08 +0900

    초기 푸시

---
+++ New Unity Project (2)/Assets/Script/Joystick.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour
+{
+    public Transform Player;
+    public Transform Stick;
+
+    private Animator ani;
+    private bool PlayerMove;
+    private Vector3 StickFirstPos;
+    private Vector3 Joyvector;
+    private float Radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Radius = GetComponent<RectTransform>().sizeDelta.y * 0.5f;
+        StickFirstPos = Stick.transform.position;
+        PlayerMove = false;
+        ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (PlayerMove) Player.transform.Translate(Vector3.forward * Time.deltaTime * 10f);
+        ani.SetBool("Iswalking", true);
+    }
+
+
+    public void Drag(BaseEventData _Data)
+    {
+        PointerEventData Data = _Data as PointerEventData;
+        Vector3 Pos = Data.position;
+        Joyvector = (Pos - StickFirstPos).normalized;
+        float Dis = Vector3.Distance(Pos, StickFirstPos);
+
+        if (Dis < Radius)
+            Stick.position = StickFirstPos + Joyvector * Dis;
+
+        else
+            Stick.position = StickFirstPos + Joyvector * Radius;
+        Player.eulerAngles = new Vector3(0, Mathf.Atan2(Joyvector.x, Joyvector.y) * Mathf.Rad2Deg, 0);
+    }
+
+    public void DragEnd()
+    {
+        Stick.position = StickFirstPos; 
+        Joyvector = Vector3.zero;
+        PlayerMove = false;
+        ani.SetBool("Iswalking", false);
+    }
+
+}
---
+++ New Unity Project (2)/Assets/Script/PlayerAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private bool AttackButton = false;
+    private Animator ani;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Click(BaseEventData _Data)
+    {
+        if(AttackButton == true)
+        {
+            ani.SetBool("Ispunching", true);
+        }
+        else if(AttackButton == false)
+        {
+            ani.SetBool("Ispunching", false);
+        }
+    }
+}
